5ca5f7fdd50bccc4db0163aaeec910d6f65a1254
narendra-itexchange <narendra.sharma@itexchangeweb.com>
2018-08-12 12:26:52 +0530

    Introduction to service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 import{Hero} from '../hero'
 
 @Component({
@@ -8,17 +8,25 @@ import{Hero} from '../hero'
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero:Hero
 
   onSelect(hero:Hero){
     this.selectedHero=hero;
   }
+  getHeroes(): void {
+  this.heroes = this.heroService.getHeroes();
+}
 
 
-  constructor() { }
+  constructor(private heroService: HeroService) { 
 
-  ngOnInit() {
+    
   }
 
+  ngOnInit() {
+  
+  this.getHeroes();
+
+}
 }
